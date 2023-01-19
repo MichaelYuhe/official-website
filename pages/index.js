59842ef9cee78d3af9429b7ad6aa714406d602a7
cosmos-explorer <lian.yang.work@gmail.com>
2023-01-19 21:32:24 +0100

    first version

--- pages/index.js
+++ pages/index.js
@@ -12,29 +12,13 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <h1>
+          FeatBit
+        </h1>
         <p>
-          Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
-          translator (English/Japanese). You can contact me on{' '}
-          <a href="https://twitter.com/chibicode">Twitter</a>.
+            FeatBit is a scalable, high-performance, and 100% open-source feature flags management platform that empowers all teams to deliver,
+            control, experiment with and monetize their software. Please visit our <a href="https://github.com/featbit/featbit" target="_blank">Github</a> repository.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   )
