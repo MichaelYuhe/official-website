5cddf24eba40e20b05e50f5dee6dc976eb3ef524
cosmos-explorer <lian.yang.work@gmail.com>
2023-01-25 10:16:37 +0100

    set view port

--- components/layout.js
+++ components/layout.js
@@ -7,29 +7,30 @@ import Link from 'next/link'
 const name = 'Shu Uesugi'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-          <script src="/public/header.js"></script>
-      </Head>
-        <div className={styles.section1}>
-            <h2>My Header</h2>
+export default function Layout({children, home}) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <script src="/public/header.js"></script>
+            </Head>
+            <div className={styles.section1}>
+                <h2>My Header</h2>
+            </div>
+            <main>{children}</main>
         </div>
-      <main>{children}</main>
-    </div>
-  )
+    )
 }
