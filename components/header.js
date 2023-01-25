cf32bb2cfadf763635f0dd4148c48aadf02cf88b
cosmos-explorer <lian.yang.work@gmail.com>
2023-01-25 10:37:43 +0100

    created header component

---
+++ components/header.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Header({ title }) {
+    return (
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="icon" href="/favicon.svg"/>
+            <meta name="og:title" content={title}/>
+            <title>{title}</title>
+        </Head>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
@@ -8,9 +8,6 @@ import Date from '../components/date'
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
