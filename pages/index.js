2e9cf9516c170a281100c3726346c68b2abd8ff8
cosmos-explorer <lian.yang.work@gmail.com>
2023-02-02 09:40:10 +0100

    typo fix

--- pages/index.js
+++ pages/index.js
@@ -83,10 +83,10 @@ export default function Home() {
       </section>
       <section className={`${styles.section2}`}>
         <div className={`container-xxl ${styles.section} py-4 py-md-5`}>
-          <div className={'h1 mb-4'}>On-Premise and upcoming Cloud solution</div>
+          <div className={'h1 mb-4'}>On-Premise and upcoming cloud solution</div>
           <div>
             <div>The on-premise option gives you the whole freedom to install FeatBit in any environment, you keep all control and own all your data.</div>
-            <div>And the upcoming Cloud solution makes it even easier to use with reasonable cost for small teams which don't have enough resources to maintain the system.</div>
+            <div>And the upcoming cloud solution makes it even easier to use with reasonable cost for small teams which don't have enough resources to maintain the system.</div>
           </div>
         </div>
       </section>
@@ -108,7 +108,7 @@ export default function Home() {
           <div className={'h2 mb-4'}>Think for developers</div>
           <div>
             Supporting a bunch of server-side and client-side SDKS, and much more on the way, FeatBit offers a very easy way to work within any tech stack.
-            The near real time data sync mode makes a flag change in effect as soon as you save it in the interface. The schedule feature can take effect at any wished time even when the whole teams is sleeping.
+            The near real time data sync mode makes a flag change in effect as soon as you save it in the interface. The schedule feature can make a change in effect at any wished time even when the whole teams is sleeping.
             Check our SDKs <Link href="https://featbit.gitbook.io/docs/getting-started/4.-connect-an-sdk" target="_blank">here</Link>
           </div>
         </div>
@@ -118,7 +118,7 @@ export default function Home() {
           <div className={'mb-4'}>QA TEAMS</div>
           <div className={'h2 mb-4'}>Test in production</div>
           <div>
-            Test in production gives the best way to test a new feature in a real environment, you test the feature the way real users use your APP. A bug can be very easy to reproduce which makes the communication between testers and devs more efficiently and effectively.
+            Test in production gives the best way to test a new feature in a real environment, you test the feature the way real users use your APP. A bug can be very easy to be reproduced which makes the communication between testers and devs more efficient and effective.
           </div>
         </div>
       </section>
@@ -127,7 +127,7 @@ export default function Home() {
           <div className={'mb-4'}>PRODUCT TEAMS</div>
           <div className={'h2 mb-4'}>Progressive release and enterprise ready experimentation</div>
           <div>
-            Product teams can release a feature to individual segments and users, rollback whenever a bug found and make it generally available when all tests are set.
+            Product teams can release a feature to individual segments and users, rollback whenever a bug is found and make it generally available when all tests are set.
             A robust statistic engine and configurable metric library means you can customize your experimentation to exactly match your needs.
           </div>
           <div className={`mt-5 ${styles.imgContainer}`}>
@@ -141,7 +141,7 @@ export default function Home() {
           <div className={'h2 mb-4'}>Understand how your users use your APP</div>
           <div>
             You have the power to see how different versions of a feature is distributed among your users and even get the version a very specific user is using.
-            Don't need to use a third-party tool to analyse how the APP is used.
+            No need to use a third-party tool to analyse how your APP is used.
           </div>
           <div className={`row mt-5`}>
             <div className={`mt-1 col-sm-12 col-md-6 ${styles.imgContainer}`}>
