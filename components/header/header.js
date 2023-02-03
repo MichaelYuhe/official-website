53dbe628265541a5421e939a54e71a76b2554255
cosmos-explorer <lian.yang.work@gmail.com>
2023-02-03 20:44:42 +0100

    added google analytics

--- components/header/header.js
+++ components/header/header.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from 'next/script'
 
 export default function Header({ title }) {
     return (
@@ -7,6 +8,15 @@ export default function Header({ title }) {
             <link rel="icon" href="/favicon.svg"/>
             <meta name="og:title" content={title}/>
             <title>{title}</title>
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZY6C6DXDBW"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+
+                gtag('config', 'G-ZY6C6DXDBW');
+            </script>
         </Head>
     );
 }
